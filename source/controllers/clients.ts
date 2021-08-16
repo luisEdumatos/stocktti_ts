dace8f7f3ab47dcb22a51595dcca1932edea4db8
luis.eduardo <luisedu.unifal@gmail.com>
2021-08-16 10:34:18 -0300

    Finish Crud

--- source/controllers/clients.ts
+++ source/controllers/clients.ts
@@ -30,4 +30,42 @@ const updateClient = async (req: Request, res: Response, next: NextFunction) => 
     let name: string = req.body.name ?? null; 
     let cnpj: string = req.body.cnpj ?? null; 
     let address: string = req.body.address ?? null; 
+
+    let response: AxiosResponse = await axios.put(`https:http://localhost:8080/api/v1/client/${id}`, {
+        ...(name && { name }),
+        ...(cnpj && { cnpj }),
+        ...(address && { address })
+    });
+
+    return res.status(200).json({
+        message: response.data
+    })
 }
+
+const deleteClient = async (req: Request, res: Response, next: NextFunction) => { 
+    let id: string = req.params.id; 
+
+    let response: AxiosResponse = await axios.delete(`https:http://localhost:8080/api/v1/client/${id}`);
+
+    return res.status(200).json({
+        message: `Client with id ${id} deleted successfully`
+    });
+}; 
+
+const addClient = async (req: Request, res: Response, next: NextFunction) => { 
+    let name: string = req.body.name; 
+    let cnpj: string = req.body.cnpj;
+    let address: string = req.body.address; 
+
+    let response: AxiosResponse = await axios.post(`https:http://localhost:8080/api/v1/client`, {
+        name,
+        cnpj,
+        address
+    }); 
+
+    return res.status(200).json({
+        message: response.data
+    }); 
+};
+
+export default {getClients, getClient, updateClient, deleteClient, addClient};
